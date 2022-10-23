c6c14886625ada292182d827451c7473f481a251
Alain Campoverde <andresarcangel00@gmail.com>
2022-10-23 17:53:17 -0500

    eventos

---
+++ js/eventos/app.js
@@ -0,0 +1,33 @@
+$(document).ready(() => {
+    /*pictograma */
+    $("#icon2").on("click", () => { cambiarPrictograma("") });
+    $("#icon3").on("click", () => { cambiarPrictograma("") });
+    $("#icon4").on("click", () => { cambiarPrictograma("") });
+    $("#icon5").on("click", () => { cambiarPrictograma("") });
+    $("#icon6").on("click", () => { cambiarPrictograma("") });
+    $("#icon7").on("click", () => { cambiarPrictograma("") });
+    $("#icon8").on("click", () => { cambiarPrictograma("") });
+    $("#icon9").on("click", () => { cambiarPrictograma("") });
+    $("#icon10").on("click", () => { cambiarPrictograma("") });
+    $("#icon11").on("click", () => { cambiarPrictograma("") });
+
+    /*texto */
+    // $("#txt_texto").on("click", () => { });
+
+    /*botones cambio de fuente */
+    $("#cambiarfuente1").on("click", () => { cambiarfuente("") });
+    $("#cambiarfuente2").on("click", () => { cambiarfuente("") });
+    $("#cambiarfuente3").on("click", () => { cambiarfuente("") });
+    $("#cambiarfuente4").on("click", () => { cambiarfuente("") });
+    $("#cambiarfuente5").on("click", () => { cambiarfuente("") });
+    $("#cambiarfuente6").on("click", () => { cambiarfuente("") });
+
+});
+
+const cambiarPrictograma = (parametro) => {
+    console.log("entra");
+};
+
+const cambiarfuente = (parametro) => {
+    console.log("entra");
+};
