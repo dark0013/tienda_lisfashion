53131279dd12c1e7360ef1a3820d38ca11b9d680
Alain Campoverde <andresarcangel00@gmail.com>
2022-10-23 19:10:26 -0500

    fuente y pictograma

--- js/eventos/app.js
+++ js/eventos/app.js
@@ -1,33 +1,48 @@
 $(document).ready(() => {
-    /*pictograma */
-    $("#icon2").on("click", () => { cambiarPrictograma("") });
-    $("#icon3").on("click", () => { cambiarPrictograma("") });
-    $("#icon4").on("click", () => { cambiarPrictograma("") });
-    $("#icon5").on("click", () => { cambiarPrictograma("") });
-    $("#icon6").on("click", () => { cambiarPrictograma("") });
-    $("#icon7").on("click", () => { cambiarPrictograma("") });
-    $("#icon8").on("click", () => { cambiarPrictograma("") });
-    $("#icon9").on("click", () => { cambiarPrictograma("") });
-    $("#icon10").on("click", () => { cambiarPrictograma("") });
-    $("#icon11").on("click", () => { cambiarPrictograma("") });
+    /*colores de camisa */
+    $("#color-azul").on("click", () => { cambiarCamisaColor("camiseta1.png") });
+    $("#color-negro").on("click", () => { cambiarCamisaColor("camiseta2.png") });
+    $("#color-rosado").on("click", () => { cambiarCamisaColor("camiseta7.png") });
+    $("#color-gis").on("click", () => { cambiarCamisaColor("camisagris.png") });
+    $("#color-blanca").on("click", () => { cambiarCamisaColor("camisablanca.png") });
 
-    /*texto */
-    // $("#txt_texto").on("click", () => { });
+
+
+    /*pictograma */
+    $("#icon2").on("click", () => { cambiarPictograma("icon2.svg") });
+    $("#icon3").on("click", () => { cambiarPictograma("icon3.svg") });
+    $("#icon4").on("click", () => { cambiarPictograma("icon4.svg") });
+    $("#icon5").on("click", () => { cambiarPictograma("icon5.svg") });
+    $("#icon6").on("click", () => { cambiarPictograma("icon6.svg") });
+    $("#icon7").on("click", () => { cambiarPictograma("icon7.svg") });
+    $("#icon8").on("click", () => { cambiarPictograma("icon8.svg") });
+    $("#icon9").on("click", () => { cambiarPictograma("icon9.svg") });
+    $("#icon10").on("click", () => { cambiarPictograma("icon10.svg") });
+    $("#icon11").on("click", () => { cambiarPictograma("icon11.svg") });
 
     /*botones cambio de fuente */
-    $("#cambiarfuente1").on("click", () => { cambiarfuente("") });
-    $("#cambiarfuente2").on("click", () => { cambiarfuente("") });
-    $("#cambiarfuente3").on("click", () => { cambiarfuente("") });
-    $("#cambiarfuente4").on("click", () => { cambiarfuente("") });
-    $("#cambiarfuente5").on("click", () => { cambiarfuente("") });
-    $("#cambiarfuente6").on("click", () => { cambiarfuente("") });
+    $("#cambiarfuente1").on("click", () => { cambiarfuente("Myriad-Pro") });
+    $("#cambiarfuente2").on("click", () => { cambiarfuente("Conduit-ITC") });
+    $("#cambiarfuente3").on("click", () => { cambiarfuente("Courier-New") });
+    $("#cambiarfuente4").on("click", () => { cambiarfuente("Arial") });
+    $("#cambiarfuente5").on("click", () => { cambiarfuente("Georgia") });
+    $("#cambiarfuente6").on("click", () => { cambiarfuente("Helvetica") });
 
 });
 
-const cambiarPrictograma = (parametro) => {
+const cambiarCamisaColor = (parametro) => { 
     console.log("entra");
+   // $('#previsualizador-imagen').css('background-color', '#FF4000');
+   // $("#previsualizador-imagen"). css('previsualizador-imagen', 'url("https://vignette.wikia.nocookie.net/despicableme/images/0/06/Daveault2.png/revision/latest?cb=20130911160410")');
+    $("#previsualizador-imagen").css("background-image", "../../assets/img/camisagris.png");//
+    $("#campotexto").attr(".xprevisualizador-imagen") ;
+};
+
+const cambiarPictograma = (parametro) => {
+    $("#previsualizador-logotipo").empty().append(`<img src="assets/icons/${parametro}" id="icon2" height="20px" width="20px" alt=""></img>`);
 };
 
 const cambiarfuente = (parametro) => {
-    console.log("entra");
+    $("#previsualizador-logotipo").empty().append(`<p class="${parametro}">${$("#txt_texto").val()}</p>`);
 };
+
